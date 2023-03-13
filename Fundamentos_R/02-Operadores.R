205e10523be40290abfed9c354ec44860d31dded
Ingo Reichert Junior <80931224+ingoreichertjr@users.noreply.github.com>
2023-03-13 13:14:26 -0300

    Add files via upload

---
+++ Fundamentos_R/02-Operadores.R
@@ -0,0 +1,53 @@
+# Operadores básicos, relacionais e lógicos em R
+
+getwd()
+
+# Operadpres básicos
+
+# Soma
+5+5
+
+# Subtração
+10-5
+
+# Multiplicação
+2*2
+
+# Divsão
+10/2
+
+# Potência
+3^3
+3**3
+
+# Módulo
+16 %% 3
+
+# Operacores relacionais
+
+# Atribuindo operadores
+x = 7
+y = 5
+
+# Operadores
+x > 8
+x < 8
+x <= 8
+x >= 8
+x == 8
+x != 8
+
+# Operadores lógicos
+
+# And
+(x==8) & (x==6)
+(x==7) & (x>=5)
+(x==8) & (x==5)
+
+# Or
+(x==8) | (x>5)
+(x==8) | (x>=5)
+
+# Not
+x > 8
+print(!x > 6)
